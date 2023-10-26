5cbd050ccec0ccfe52f8a1c29cdee3aefe7b841c
llukezin <lucassmendonca77@gmail.com>
2023-10-26 17:32:02 -0300

    atividade

---
+++ exercicio01.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>atividade</title>
+
+    <style>
+        p {
+            font-family: verdana;
+        }
+    </style>
+</head>
+<body>
+    <h1>Atividade que o prof passou</h1>
+    <hr>
+
+    <?php
+    $nome = "Chapolin";
+    $idade = 25;
+
+    echo '<p>'.$nome.' tem ' .$idade.' anos</p>';
+    ?>
+
+<hr>
+
+
+
+
+</body>
+</html>
