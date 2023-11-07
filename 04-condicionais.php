fbc086d58eb04b82668e9280ba21bf591004815f
llukezin <lucassmendonca77@gmail.com>
2023-11-07 14:10:44 -0300

    css do estoque

--- 04-condicionais.php
+++ 04-condicionais.php
@@ -4,7 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Condicionais</title>
-
+<style>
+    .repor { color: red;}
+    .urgente {color: purple;}
+    .normal {color: green;}
+</style>
     
 </head>
 <body>
